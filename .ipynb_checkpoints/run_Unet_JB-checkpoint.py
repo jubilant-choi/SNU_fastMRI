15f04dcd8ba59415f1844e501169bc81a197340b
jubilant-choi <wnqlszoq123@gmail.com>
2022-08-08 18:52:13 +0000

    add varnet

--- .ipynb_checkpoints/run_Unet_JB-checkpoint.py
+++ .ipynb_checkpoints/run_Unet_JB-checkpoint.py
@@ -1,26 +1,21 @@
 import subprocess
 from datetime import datetime
 
-user = 'JB'
-model = 'newUnet'
-batch = 12
-epoch = 30
-lrs = [1e-3]”
-base_exp_name = 'newUnet_test'
+model = 'VarNet'
+lrs = [1e-3,1e-4,1e-5,1e-6]
+base_exp_name = 'VarNet_test'
 
 for i in range(len(lrs)):
-    curtime = datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
-    exp_num = '0'+str(i+1) if i <= 8 else str(i+1)
-    exp_name = base_exp_name+exp_num
     lr = lrs[i]
-    command = f'python ./train.py -u {user} -x {exp_name} -n {model} -e {epoch} -b {batch} -l {lr}'
+    curtime = datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
+    exp_name = base_exp_name+str(lr)
+    command = f'python ./train.py -u JB -x {exp_name} -n {model} -b 1 -e 3 -l {lr} -s P --input-key kspace --cascade 4'
     print(command)
-#     result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#     if result.stderr != '':
-#         print("ERROR OCCURED")
-#         print(result.stderr.decode('utf-8'))
-#         break
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.stderr != '':
+        print("ERROR OCCURED")
+        print(result.stderr.decode('utf-8'))
+        break
     
-#     with open(f'./logs/{user}/base_exp_name_{curtime}.txt', 'w') as f:
-#         f.write(result.stdout.decode('utf-8'))
-#     break 
+    with open(f'./logs/{user}/base_exp_name_{curtime}.txt', 'w') as f:
+        f.write(result.stdout.decode('utf-8'))
--- train.py
+++ train.py
@@ -1,6 +1,11 @@
+import os, sys
+if os.getcwd() + '/utils/model/' not in sys.path:
+    sys.path.insert(1, os.getcwd() + '/utils/model/')
+
 import argparse
 import shutil
 import hashlib
+
 from utils.learning.train_part import train
 from utils.common.utils import save_exp_result
 from pathlib import Path
@@ -13,12 +18,13 @@ def parse():
     parser.add_argument('-e', '--num-epochs', type=int, default=3, help='Number of epochs')
     parser.add_argument('-l', '--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('-r', '--report-interval', type=int, default=10, help='Report interval')
-    parser.add_argument('-n', '--net-name', type=Path, default='test_Unet', help='Name of network')
+    parser.add_argument('-n', '--net-name', type=Path, required=True, help='Name of network')
     parser.add_argument('-o', '--optim', type=str, default='Adam', help='Name of optimizer')
     parser.add_argument('-s', '--scheduler', type=str, default='Plateau', help='Name of lr scheduler')
     
-    parser.add_argument('-t', '--data-path-train', type=Path, default='/root/input/train/image/', help='Directory of train data')
-    parser.add_argument('-v', '--data-path-val', type=Path, default='/root/input/val/image/', help='Directory of validation data')
+    parser.add_argument('-t', '--data-path-train', type=Path, default='/root/input/train/', help='Directory of train data')
+    parser.add_argument('-v', '--data-path-val', type=Path, default='/root/input/val/', help='Directory of validation data')
+    parser.add_argument('--cascade', type=int, default=1, help='Number of cascades | Should be less than 12')
     
     parser.add_argument('--in-chans', type=int, default=1, help='Size of input channels for network')
     parser.add_argument('--out-chans', type=int, default=1, help='Size of output channels for network')
@@ -29,12 +35,10 @@ def parse():
     parser.add_argument('--load', type=str, default='', help='Name of saved model that will be loaded')
     parser.add_argument('-u', '--user', type=str, choices=['SJ','JB'], required=True, help='User name')
     parser.add_argument('-x', '--exp-name', type=str, default='test', help='Name of an experiment')
-    
       
     args = parser.parse_args()
-    
-    tot_iter = 5164
-    args.report_interval = int((tot_iter/args.batch_size)/10)
+#     tot_iter = 5164
+#     args.report_interval = int((tot_iter/args.batch_size)/10)
     
     return args
 

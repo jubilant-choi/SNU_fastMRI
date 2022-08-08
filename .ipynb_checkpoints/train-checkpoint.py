5e50b21927737e0e68d0f43b595b139d8df7812a
jubilant-choi <wnqlszoq123@gmail.com>
2022-08-08 09:54:25 +0000

    set repo again

---
+++ .ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,63 @@
+import argparse
+import shutil
+import hashlib
+from utils.learning.train_part import train
+from utils.common.utils import save_exp_result
+from pathlib import Path
+
+def parse():
+    parser = argparse.ArgumentParser(description='Train Unet on FastMRI challenge Images',
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--GPU-NUM', type=int, default=0, help='GPU number to allocate')
+    parser.add_argument('-b', '--batch-size', type=int, default=256, help='Batch size')
+    parser.add_argument('-e', '--num-epochs', type=int, default=3, help='Number of epochs')
+    parser.add_argument('-l', '--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('-r', '--report-interval', type=int, default=10, help='Report interval')
+    parser.add_argument('-n', '--net-name', type=Path, default='test_Unet', help='Name of network')
+    parser.add_argument('-o', '--optim', type=str, default='Adam', help='Name of optimizer')
+    parser.add_argument('-s', '--scheduler', type=str, default='Plateau', help='Name of lr scheduler')
+    
+    parser.add_argument('-t', '--data-path-train', type=Path, default='/root/input/train/image/', help='Directory of train data')
+    parser.add_argument('-v', '--data-path-val', type=Path, default='/root/input/val/image/', help='Directory of validation data')
+    
+    parser.add_argument('--in-chans', type=int, default=1, help='Size of input channels for network')
+    parser.add_argument('--out-chans', type=int, default=1, help='Size of output channels for network')
+    parser.add_argument('--input-key', type=str, default='image_input', help='Name of input key')
+    parser.add_argument('--target-key', type=str, default='image_label', help='Name of target key')
+    parser.add_argument('--max-key', type=str, default='max', help='Name of max key in attributes')
+    
+    parser.add_argument('--load', type=str, default='', help='Name of saved model that will be loaded')
+    parser.add_argument('-u', '--user', type=str, choices=['SJ','JB'], required=True, help='User name')
+    parser.add_argument('-x', '--exp-name', type=str, default='test', help='Name of an experiment')
+    
+      
+    args = parser.parse_args()
+    
+    tot_iter = 5164
+    args.report_interval = int((tot_iter/args.batch_size)/10)
+    
+    return args
+
+if __name__ == '__main__':
+    args = parse()
+    args.exp_dir = './result' / Path(args.user) / args.net_name / 'checkpoints'
+    args.val_dir = './result' / Path(args.user) / args.net_name / 'reconstructions_val'
+    args.json_dir = './result' / Path(args.user) / args.net_name / 'jsons'
+    args.main_dir = './result' / Path(args.user) / args.net_name / __file__
+
+    args.exp_dir.mkdir(parents=True, exist_ok=True)
+    args.val_dir.mkdir(parents=True, exist_ok=True)
+    print(f"*** Experiment <{args.exp_name}> with model <{args.net_name}> starts ***")
+    train(args)
+    
+# def run():
+#     args = parse()
+#     args.exp_dir = './result' / Path(args.user) / args.net_name / 'checkpoints'
+#     args.val_dir = './result' / Path(args.user) / args.net_name / 'reconstructions_val'
+#     args.json_dir = './result' / Path(args.user) / args.net_name / 'jsons'
+#     args.main_dir = './result' / Path(args.user) / args.net_name / __file__
+
+#     args.exp_dir.mkdir(parents=True, exist_ok=True)
+#     args.val_dir.mkdir(parents=True, exist_ok=True)
+
+#     train(args)
---
+++ utils/common/utils.py
@@ -0,0 +1,99 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+import os
+from skimage.metrics import structural_similarity
+from math import sqrt
+from pathlib import Path
+import json
+import h5py
+import numpy as np
+
+def save_reconstructions(reconstructions, out_dir, targets=None, inputs=None):
+    """
+    Saves the reconstructions from a model into h5 files that is appropriate for submission
+    to the leaderboard.
+
+    Args:
+        reconstructions (dict[str, np.array]): A dictionary mapping input filenames to
+            corresponding reconstructions (of shape num_slices x height x width).
+        out_dir (pathlib.Path): Path to the output directory where the reconstructions
+            should be saved.
+        target (np.array): target array
+    """
+    out_dir.mkdir(exist_ok=True, parents=True)
+    for fname, recons in reconstructions.items():
+        with h5py.File(out_dir / fname, 'w') as f:
+            f.create_dataset('reconstruction', data=recons)
+            if targets is not None:
+                f.create_dataset('target', data=targets[fname])
+            if inputs is not None:
+                f.create_dataset('input', data=inputs[fname])
+
+def ssim_loss(gt, pred, maxval=None):
+    """Compute Structural Similarity Index Metric (SSIM)
+       ssim_loss is defined as (1 - ssim)
+    """
+    maxval = gt.max() if maxval is None else maxval
+    ssim = 0
+    for slice_num in range(gt.shape[0]):
+        ssim = ssim + structural_similarity(
+            gt[slice_num], pred[slice_num], data_range=maxval
+        )
+
+    ssim = ssim / gt.shape[0]
+    return 1 - ssim
+
+def fftc(data, axes=(-2, -1), norm="ortho"):
+    """
+    Centered fast fourier transform
+    """
+    return np.fft.fftshift(
+        np.fft.fftn(np.fft.ifftshift(data, axes=axes), 
+                    axes=axes, 
+                    norm=norm), 
+        axes=axes
+    )
+
+
+def ifftc(data, axes=(-2, -1), norm="ortho"):
+    """
+    Centered inverse fast fourier transform
+    """
+    return np.fft.fftshift(
+        np.fft.ifftn(np.fft.ifftshift(data, axes=axes), 
+                     axes=axes, 
+                     norm=norm), 
+        axes=axes
+    )
+
+def rss_combine(data, axis, keepdims=False):
+    return np.sqrt(np.sum(np.square(np.abs(data)), axis, keepdims=keepdims))
+
+def save_exp_result(save_dir, setting, result, load=''):
+    makedir(save_dir)
+    for key in setting.copy():
+        if isinstance(setting[key],Path):
+            setting[key]=str(setting[key])
+            
+    exp_name = setting['exp_name']
+    filename = save_dir / '{}.json'.format(exp_name)
+    
+    if load != '':
+        with open(filename, 'r') as f:
+            prev_result = json.load(f)
+        result['train_losses'] = prev_result['train_losses'] + [result['train_losses'][-1]]
+        result['val_losses'] = prev_result['val_losses'] + [result['val_losses'][-1]]
+        
+    result.update(setting)
+
+    with open(filename, 'w') as f:
+        json.dump(result, f)
+
+
+def makedir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
